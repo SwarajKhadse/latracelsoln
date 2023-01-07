fece786e85c9c9248d3cf5997f04f3fccdbba6b6
SwarajKhadse <khadseswaraj@gmail.com>
2023-01-07 09:33:57 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import StudentAttendance from './Components';
+
+function App() {
+  return (
+    <div className="App">
+    <StudentAttendance></StudentAttendance>
+    </div>
+  );
+}
+
+export default App;
